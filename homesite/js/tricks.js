f0fa345d2ac2d8a60554ec09ea882ce59a9c0fd8
Claudiu Moise <claudiu.moise@gmail.com>
2017-12-28 17:23:35 -0600

    Website building

--- homesite/js/tricks.js
+++ homesite/js/tricks.js
@@ -2,3 +2,15 @@
 function killpopup() {
     document.getElementById('popup').style.display = 'none';
 }
+
+
+function statusCheck() {
+				hidden = !hidden;
+				if(hidden) {
+					document.getElementById('b1').style.visibility = 'hidden';
+					document.getElementById('b2').style.visibility = 'hidden';
+				} else {
+					document.getElementById('b1').style.visibility = 'visible';
+					document.getElementById('b2').style.visibility = 'visible';
+				}
+			}
